3d3461bc1cabe11ca494da28e627549e67029005
Soitch <virtsoi99@gmail.com>
2022-05-03 17:04:35 +0300

    Add workbot

--- bot-project/bot.py
+++ bot-project/bot.py
@@ -1,14 +1,15 @@
 import config
 import telebot
-import os
-import time
 
+# Создаем экземпляр бота
 bot = telebot.TeleBot(config.token)
-
+# Функция, обрабатывающая команду /start
+@bot.message_handler(commands=["start"])
+def start(m, res=False):
+    bot.send_message(m.chat.id, 'Я на связи. Напиши мне что-нибудь )')
+# Получение сообщений от юзера
 @bot.message_handler(content_types=["text"])
-def repeat_all_messages(message): # Название функции не играет никакой роли
-    bot.send_message(message.chat.id, message.text)
-
-
-if __name__ == '__main__':
-    bot.infinity_polling()
+def handle_text(message):
+    bot.send_message(message.chat.id, 'Вы написали: ' + message.text)
+# Запускаем бота
+bot.polling(none_stop=True, interval=0)
